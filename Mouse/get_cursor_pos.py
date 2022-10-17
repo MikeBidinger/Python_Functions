3fd37326627ce46b579dad89463dcf49e2d5a2fa
Mike Bidinger <114107131+MikeBidinger@users.noreply.github.com>
2022-10-17 16:13:06 +0200

    Add files via upload

---
+++ Mouse/get_cursor_pos.py
@@ -0,0 +1,19 @@
+from pprint import pprint as pp
+import win32gui
+import keyboard
+
+print('\nGet Cursor Program active.')
+print('Press Q to exit.')
+print('Press G to get the mouse posistion.\n')
+
+while True:
+    try:
+        if keyboard.is_pressed('q'):
+            print('Exit program...')
+            break
+        elif keyboard.is_pressed('g'):
+            pp(win32gui.GetCursorPos())
+            while keyboard.is_pressed('g'):
+                pass
+    except:
+        break
