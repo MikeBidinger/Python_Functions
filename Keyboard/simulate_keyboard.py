f1fdf74e8df66f0a1160f523d43d22df60075c93
Mike Bidinger <114107131+MikeBidinger@users.noreply.github.com>
2022-10-17 16:16:11 +0200

    Add files via upload

---
+++ Keyboard/simulate_keyboard.py
@@ -0,0 +1,15 @@
+from pprint import pprint as pp
+import os
+import keyboard
+import time
+
+os.startfile(r'C:\Windows\System32\notepad.exe')
+
+print('Enter Sleep')
+time.sleep(2)
+print('Exit Sleep')
+
+# Keyboard Control
+inputs = ['q', 'x', 'q', '8', '*', 'enter', 'tab', 'shift+a', 'ctrl+a']
+for x in inputs:
+    keyboard.press_and_release(x)
