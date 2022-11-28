883ac20746bf1065270d7100965a303d773c5f72
Mike Bidinger <114107131+MikeBidinger@users.noreply.github.com>
2022-11-28 16:00:53 +0100

    Update json_functions.py

--- json_functions.py
+++ json_functions.py
@@ -2,18 +2,10 @@ import json
 from pprint import pprint as pp
 
 def write_json(file_path:str, data):
-    f = open(file_path, 'w')
-    # pp(data)
-    js_str = json.dumps(data, indent=4)
-    #print(js_str)
-    f.write(js_str)
-    f.close()
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
 
 def read_json(file_path:str):
-    f = open(file_path, 'r')
-    js_str = f.read()
-    #print(js_str)
-    data = json.loads(js_str)
-    #pp(data)
-    f.close()
+    with open(file_path, 'r') as f:
+        data = json.load(f)
     return data
