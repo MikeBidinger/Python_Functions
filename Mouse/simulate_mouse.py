e6d1cb2ae6e52aeb6f9898d421defdebdcae4229
MikeBidingerCDG <mike.bidinger@connecteddatagroup.nl>
2024-07-31 12:43:25 +0200

    Update simulate_mouse.py

--- Mouse/simulate_mouse.py
+++ Mouse/simulate_mouse.py
@@ -1,28 +1,33 @@
 import pyautogui  # https://pyautogui.readthedocs.io/ - This module is not build-in with Python
+import time
 
 
 def main():
-    get_mouse_position()
+    get_mouse_position(True)
     set_mouse(650, 400)
     move_mouse(300, 300, 1)
-    afk_movement(100, 100, 5)
+    afk_movement(100, 100, (15 * 60))
 
 
-def get_mouse_position() -> tuple[int, int]:
+def get_mouse_position(user_confirm: bool = False) -> tuple[int, int]:
     """Get the current mouse position coordinates.
+    - Args:
+        - user_confirm: An optional boolean representing wether or not the user should confirm to return the current mouse position coordinates.
     - Returns:
         - A tuple containing the current mouse position x- and y-coordinate.
     """
-    # Print screen resolution
-    SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
-    print(
-        f"Screen resolution:\n\tWidth :{SCREEN_WIDTH:>5}\n\tHeight:{SCREEN_HEIGHT:>5}"
-    )
-    # Wait for user input (Enter)
-    input("Press enter to register the current mouse position (coordinates):")
+    if user_confirm:
+        # Print screen resolution
+        SCREEN_WIDTH, SCREEN_HEIGHT = pyautogui.size()
+        print(
+            f"Screen resolution:\n\tWidth :{SCREEN_WIDTH:>5}\n\tHeight:{SCREEN_HEIGHT:>5}"
+        )
+        # Wait for user input (Enter)
+        input("Press enter to register the current mouse position (coordinates):")
     # Print the current mouse location coordinates
     current_mouse_x, current_mouse_y = pyautogui.position()
-    print(f"\tX:{current_mouse_x:>5}\n\tY:{current_mouse_y:>5}")
+    if user_confirm:
+        print(f"\tX:{current_mouse_x:>5}\n\tY:{current_mouse_y:>5}")
     return (current_mouse_x, current_mouse_y)
 
 
@@ -45,13 +50,16 @@ def move_mouse(x: int, y: int, duration: float = 0, click: bool = False):
     """Move the mouse position relative to the current mouse position.
     NOTE: The top-left corner of the screen represents the (0, 0) coordinates.
     - Args:
-        - x: An integer representing the x-coordinate on the screen.
-        - y: An integer representing the y-coordinate on the screen.
+        - x: An integer representing the x-coordinate to be moved on the screen.
+        - y: An integer representing the y-coordinate to be moved on the screen.
         - duration: An optional float representing the duration of the mouse movement in seconds.
         - click: An optional boolean representing wether or not the mouse should be clicked on the given position.
+    - Returns:
+        - A tuple containing the current mouse position x- and y-coordinate.
     """
     # Move mouse relatively to the current position
     pyautogui.move(x, y, duration, pyautogui.easeInOutQuad)
+    return get_mouse_position()
 
 
 def afk_movement(x: int, y: int, duration: int):
@@ -65,12 +73,19 @@ def afk_movement(x: int, y: int, duration: int):
     print("AFK script running...")
     # Set the starting mouse position
     set_mouse(x, y)
+    mouse_pos = get_mouse_position()
     # For the given duration:
     for i in range(duration):
+        if get_mouse_position() != mouse_pos:
+            break
         # Move the mouse by one pixel over one second
-        move_mouse(1, 0, 1)
+        mouse_pos = move_mouse(1, 0)
         print(f"{i + 1} / {duration} seconds")
-    print("AFK script finished!")
+        time.sleep(1)
+    else:
+        print("AFK script finished!")
+        return
+    print("AFK script stopped!")
 
 
 if __name__ == "__main__":
